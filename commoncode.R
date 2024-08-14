38a52c7bf167c789f300038c3d313e85b215af37
MurrayEfford <murray.efford@otago.ac.nz>
2024-08-14 16:37:18 +1200

    commoncode now before_chapter_script; indexing

--- commoncode.R
+++ commoncode.R
@@ -1,12 +1,12 @@
 # code run for each chapter with R chunks
 
-# It is not strictly necessary to run this in every chapter after the first, but it makes 
-# debugging easier, as each chapter may be knitted independently
+# It is not strictly necessary to run this in every chapter after the first, 
+# but it makes debugging easier, as each chapter may be knitted independently
 
 library(secr)
 setNumThreads(18)
 hareCH6 <- read.capthist("data/hareCH6capt.txt", "data/hareCH6trap.txt", detector = "single")
-options(digits = 5, width = 85)       
+options(digits = 5, width = 70)  # 85       
 
 # from RMarkdown cookbook Yihui Xie, Christophe Dervieux, Emily Riederer
 # 2023-12-30 Chapman & Hall/CRC
